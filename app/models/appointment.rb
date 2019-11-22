6d961c75427f651e0a78758ad5c097f4e132116b
Dominique Skaf <doskaf@yahoo.com>
2019-11-22 15:54:59 -0700

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :patient
+
+  def readable_datetime
+    self.appointment_datetime.strftime( "%B %e, %Y at %H:%M")
+  end
+
+end
