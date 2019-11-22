7ea782be2cfc8001dd1c96adfd1978d7838c80ea
samirv16 <samir.vallecillo@outlook.com>
2019-11-21 18:38:29 -0600

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment < ApplicationRecord
+    belongs_to :patient
+    belongs_to :doctor
+  
+    def time
+      self.appointment_datetime.strftime( "%B %e, %Y at %H:%M")
+    end
+  
+  end
